57a69615b3ece24ba118c0475e2241beea0fd82c
Sophia Mersmann <sophia.mersmann1@gmail.com>
2022-02-06 12:15:21 +0100

    Remove comments

--- cypress/integration/button.spec.js
+++ cypress/integration/button.spec.js
@@ -1,25 +1,10 @@
-describe("Default button", () => {
-  beforeEach(() => {
-    cy.visit("/");
-  });
+describe('Button', () => {
+	beforeEach(() => {
+		cy.visit('/');
+	});
 
-  it("can be toggled", () => {
-    // cy.contains('Before').click().should('contain.text', 'After');
-    // let count = 0;
-    // const click = ($el) => {
-    //   count += 1;
-    //   return $el.click();
-    // };
-    // cy.contains('Before')
-    //   .pipe(click)
-    //   .should(($el) => {
-    //     expect($el).to.contain('After');
-    //   })
-    //   .then(() => {
-    //     cy.log(`clicked ${count} times`);
-    //   });
-
-    cy.contains("Show").click();
-    cy.contains("Content").should("be.visible");
-  });
+	it('can be toggled', () => {
+		cy.contains('Show').click();
+		cy.contains('Content').should('be.visible');
+	});
 });
